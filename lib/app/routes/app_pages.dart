5d110db4ba61ae1fd59436450603ff57d6e6ec67
Gaurav Joshi <joshig890@gmail.com>
2024-10-25 16:30:46 +0530

    nutritional goal

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -28,7 +28,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.GENERATE_AI;
+  static const INITIAL = Routes.NUTRITION_GOAL;
 
   static final routes = [
     GetPage(
